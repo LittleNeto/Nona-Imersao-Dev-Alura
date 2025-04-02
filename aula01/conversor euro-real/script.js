f945146ba05f07885df169d471d0dade2fde012c
José Agra Neto <agrajoseneto@gmail.com>
2025-04-02 00:01:24 -0300

    primeiro desafio realizado
    mão domino muito html e css (ainda). qualquer alteração é bem-vinda.

---
+++ aula01/conversor euro-real/script.js
@@ -0,0 +1,5 @@
+//código realizado no site codepen.io
+valor_euros = prompt("digite um valor em euros: ")
+valor_real = valor_euros * 6.1312
+valor_formatado = valor_real.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+alert(`R$${valor_formatado}`)
