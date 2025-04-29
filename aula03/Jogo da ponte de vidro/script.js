ab4d98afc55abac9ddbc83682c5da5b1e90e6b31
José Agra Neto <agrajoseneto@gmail.com>
2025-04-29 20:15:52 -0300

    Update script.js

--- aula03/Jogo da ponte de vidro/script.js
+++ aula03/Jogo da ponte de vidro/script.js
@@ -1,28 +1,30 @@
 //código realizado no site codepen.io
-let rodada = 1
-let jogar = true
-while (jogar) {
-  let confirmacao = false
-  let escolhaJogador = 0 // sem essa declaração, o popup simplesmente fechava sem executar o resto do programa (não sei se é um erro do codepen.io)
-  while (!confirmacao) {
-    escolhaJogador = Number(prompt(`Nível ${rodada}, vidro (1, 2 ou 3)?`))
-    if (escolhaJogador < 1 || escolhaJogador > 3) {
-      alert(`O número ${escolhaJogador} é inválido! Por favor, tente novamente`)
-    } else {
-      confirmacao = true
+function jogar() {
+  let rodada = 1
+  let jogar = true
+  while (jogar) {
+    let confirmacao = false
+    let escolhaJogador = 0 // sem essa declaração, o popup simplesmente fechava sem executar o resto do programa (não sei se é um erro do codepen.io)
+    while (!confirmacao) {
+      escolhaJogador = Number(prompt(`Nível ${rodada}, vidro (1, 2 ou 3)?`))
+      if (escolhaJogador < 1 || escolhaJogador > 3) {
+        alert(`O número ${escolhaJogador} é inválido! Por favor, tente novamente`)
+      } else {
+        confirmacao = true
+      }
     }
-  }
-  let pisoQuebrado = Number(Math.floor(Math.random() * 3) + 1)
-  if (escolhaJogador === pisoQuebrado) {
-    alert('Vidro quebrou! Acabou o jogo para você')
-    rodada = 4
-    jogar = false
-  } else {
-    alert(`PASSOU! O vidro quebrado estava na ponte: ${pisoQuebrado}`)
-    rodada += 1
-    if (rodada > 3) {
-      alert('Você venceu! PARABÉNS!')
+    let pisoQuebrado = Number(Math.floor(Math.random() * 3) + 1)
+    if (escolhaJogador === pisoQuebrado) {
+      alert('Vidro quebrou! Acabou o jogo para você')
+      rodada = 4
       jogar = false
+    } else {
+      alert(`PASSOU! O vidro quebrado estava na ponte: ${pisoQuebrado}`)
+      rodada += 1
+      if (rodada > 3) {
+        alert('Você venceu! PARABÉNS!')
+        jogar = false
+      }
     }
   }
 }
